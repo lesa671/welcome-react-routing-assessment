26b64f4b61bb7e4fecd516d0a8a29aaee6dbbd73
Lesa Ducksworth <lducks67@gmail.com>
2020-08-11 15:20:40 -0500

    added 404 error component and assets folder for possible image use

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Welcome from './components/welcome/Welcome';
 import Clock from './components/clock/Clock';
 import Contact from './components/contact/Contact';
 import Navigation from './/components/navigation/Navigation'
+import NoMatch from './components/noMatch/NoMatch'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Route
-      exact 
-      path='/' 
-      render={(props) => <Welcome {...props} name='Lesa Lesa' />} 
-      />
-      <Route path='/clock' component={Clock} />
-      <Route path='/contact' component={Contact} />
+
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={(props) => <Welcome {...props} name='Lesa Lesa' />}
+        />
+
+        <Route
+          path='/welcome/:name'
+          render={(props) => (<Welcome {...props} name={props.match.params.name} />)}
+        />
+
+        <Route path='/clock' component={Clock} />
+        <Route path='/contact' component={Contact} />
+
+        <Route component={NoMatch} />
+
+      </Switch>
     </div>
   );
 }
 
 export default App;
-
-{/* <Welcome name='Lesa Lesa'/>
-<Clock />
-<Contact /> */}
