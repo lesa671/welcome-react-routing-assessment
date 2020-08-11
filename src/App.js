0e1d6affde04371aa7a6694c02969409998c90d6
Lesa Ducksworth <lducks67@gmail.com>
2020-08-11 07:41:56 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 import './App.css';
 import Welcome from './components/welcome/Welcome';
 import Clock from './components/clock/Clock';
 import Contact from './components/contact/Contact';
+import Navigation from './/components/navigation/Navigation'
 
 function App() {
   return (
     <div className="App">
-      <Welcome name='Lesa Lesa'/>
-      <Clock />
-      <Contact />
+      <Navigation />
+      <Route
+      exact 
+      path='/' 
+      render={(props) => <Welcome {...props} name='Lesa Lesa' />} 
+      />
+      <Route path='/clock' component={Clock} />
+      <Route path='/contact' component={Contact} />
     </div>
   );
 }
 
 export default App;
+
+{/* <Welcome name='Lesa Lesa'/>
+<Clock />
+<Contact /> */}
